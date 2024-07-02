9d5940e489a5dac13f71710ac5b1fbe2b2bfc1a7
joao.simao <vitoralves1604@gmail.com>
2024-07-02 17:57:15 -0300

    update

---
+++ app/Pages/components/headerMenu/headerMenu.tsx
@@ -0,0 +1,11 @@
+import styles from './css/headerMenu';
+import * as React from 'react';
+import { Row } from 'react-native-simple';
+
+export default function HeaderMenu(){
+    return (
+       <Row style={styles.header}>
+            
+       </Row>
+    );
+}
